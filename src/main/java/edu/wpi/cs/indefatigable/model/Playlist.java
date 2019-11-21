a049eb2ff185b921fec2e32e50609b43819ca20c
James Plante <jplante@wpi.edu>
2019-11-21 14:24:54 -0500

    Initial function prototypes for Model Playlist class

--- src/main/java/edu/wpi/cs/indefatigable/model/Playlist.java
+++ src/main/java/edu/wpi/cs/indefatigable/model/Playlist.java
@@ -1,4 +1,48 @@
 package edu.wpi.cs.indefatigable.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.UUID;
+
 public class Playlist {
+	String puid;
+	ArrayList<Video> videos;
+	
+	// Constructors
+	Playlist(String puid, ArrayList<Video> videos) {
+		this.puid = puid;
+		this.videos = videos;
+	}
+	
+	Playlist() {
+		this.videos = new ArrayList<Video>();
+		this.puid = UUID.randomUUID().toString();
+	}
+	
+	/***
+	 * Get an iterator containing all videos in the Playlist
+	 * @return (Iterator<Video>) An iterator containing all videos in the Playlist
+	 */
+	public Iterator<Video> getAllVideos() {	
+		return this.videos.iterator();
+	}
+	
+	/***
+	 * Add video to the Playlist
+	 * @param (Video) v - Video to be added
+	 * @return (boolean) true if added, false if not
+	 */
+	public boolean addVideo(Video v) {
+		return true;
+	}
+	
+	/***
+	 * Remvoe video from the Playlist
+	 * @param (Video) v - Video to be removed
+	 * @return (boolean) true if removed, false if not
+	 */
+	public boolean removeVideo(Video v) {
+		return true;
+	}
+	
 }
