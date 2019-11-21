2b97283e34159c0e36ef9f3437e843cea4b4b563
Justin Aquilante <Justo@Justo-PC>
2019-11-21 14:38:00 -0500

    Wrote handler for ListAllPlaylists

---
+++ src/main/java/edu/wpi/cs/indefatigable/db/PlaylistDAO.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs.indefatigable.db;
+
+import java.util.ArrayList;
+
+import edu.wpi.cs.indefatigable.model.Playlist;
+
+/*
+ * so it will compile
+ * 
+ */
+public class PlaylistDAO{
+	public ArrayList<Playlist> playlists(){
+		return null;
+		} // stub
+}
+//TODO DELETE WHEN JIM PUSHES PlaylistDAO
