6d81e3c3040e052d1592a9a5c703e9a26e144ca0
James Plante <jplante@wpi.edu>
2019-11-25 14:40:37 -0500

    searchVideoSegments: Response classes done

---
+++ src/main/java/edu/wpi/cs/indefatigable/http/SearchVideosRequest.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs.indefatigable.http;
+
+public class SearchVideosRequest {
+	
+	public String transcript;
+	public String character;
+	
+	// Constructors
+	public SearchVideosRequest(String transcript, String character) {
+		this.transcript = transcript;
+		this.character = character;
+	}
+
+	public String getTranscript() {
+		return transcript;
+	}
+
+	public String getCharacter() {
+		return character;
+	}
+	
+}
---
+++ src/main/java/edu/wpi/cs/indefatigable/http/SearchVideosResponse.java
@@ -0,0 +1,15 @@
+package edu.wpi.cs.indefatigable.http;
+
+public class SearchVideosResponse {
+	
+	public int responseCode;
+
+	public SearchVideosResponse(int responseCode) {
+		this.responseCode = responseCode;
+	}
+	
+	public SearchVideosResponse() {
+		this.responseCode = responseCode;
+	}
+	
+}
