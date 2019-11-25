8706d1348557f4ad7f96c0974cad29089841fc64
James Plante <jplante@wpi.edu>
2019-11-25 15:56:26 -0500

    searchVideoSegments: Finished initial implementation, onto testing

--- src/main/java/edu/wpi/cs/indefatigable/SearchVideosHandler.java
+++ src/main/java/edu/wpi/cs/indefatigable/SearchVideosHandler.java
@@ -1,20 +1,28 @@
 package edu.wpi.cs.indefatigable;
 
+import java.util.ArrayList;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import edu.wpi.cs.indefatigable.db.VideoDAO;
 import edu.wpi.cs.indefatigable.http.SearchVideosRequest;
 import edu.wpi.cs.indefatigable.http.SearchVideosResponse;
+import edu.wpi.cs.indefatigable.model.Video;
 
 public class SearchVideosHandler implements RequestHandler<SearchVideosRequest, SearchVideosResponse>{
-	
-	public SearchVideosHandler() {
-		// TODO Auto-generated constructor stub
-	}
-	
+
+	public SearchVideosHandler() {}
+
 	@Override
 	public SearchVideosResponse handleRequest(SearchVideosRequest input, Context context) {
-		
+		try {
+			VideoDAO dao = new VideoDAO();
+			ArrayList<Video> results = dao.queryAllVideos(input.getCharacter(), input.getTranscript());
+			return new SearchVideosResponse(200, results);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new SearchVideosResponse();
+		}
 	}
-	
 }
--- src/main/java/edu/wpi/cs/indefatigable/http/SearchVideosResponse.java
+++ src/main/java/edu/wpi/cs/indefatigable/http/SearchVideosResponse.java
@@ -1,15 +1,23 @@
 package edu.wpi.cs.indefatigable.http;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.cs.indefatigable.model.Video;
+
 public class SearchVideosResponse {
 	
 	public int responseCode;
+	public List<Video> videos;
 
-	public SearchVideosResponse(int responseCode) {
+	public SearchVideosResponse(int responseCode, List<Video> videos) {
 		this.responseCode = responseCode;
+		this.videos = videos;
 	}
 	
 	public SearchVideosResponse() {
-		this.responseCode = responseCode;
+		this.responseCode = 400;
+		this.videos = new ArrayList<Video>();
 	}
 	
 }
