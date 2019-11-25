a633ff9c5b9b4085e7436e3efabe974ba7b94482
James Plante <jplante@wpi.edu>
2019-11-25 15:42:19 -0500

    searchVideoSegments: Created database function

--- src/main/java/edu/wpi/cs/indefatigable/SearchVideosHandler.java
+++ src/main/java/edu/wpi/cs/indefatigable/SearchVideosHandler.java
@@ -1,4 +1,20 @@
 package edu.wpi.cs.indefatigable;
 
-public class SearchVideosHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import edu.wpi.cs.indefatigable.http.SearchVideosRequest;
+import edu.wpi.cs.indefatigable.http.SearchVideosResponse;
+
+public class SearchVideosHandler implements RequestHandler<SearchVideosRequest, SearchVideosResponse>{
+	
+	public SearchVideosHandler() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public SearchVideosResponse handleRequest(SearchVideosRequest input, Context context) {
+		
+	}
+	
 }
