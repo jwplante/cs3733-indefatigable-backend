5d7b2028a145c2d6eea644f452432f53b8a25fef
René Borner <mail@reneborner.de>
2019-12-11 14:45:36 -0500

    Added SearchVideosHandlerTest

---
+++ src/main/test/edu/wpi/cs/indefatigable/SearchVideosHandlerTest.java
@@ -0,0 +1,30 @@
+package edu.wpi.cs.indefatigable;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import edu.wpi.cs.indefatigable.http.SearchVideosRequest;
+import edu.wpi.cs.indefatigable.http.SearchVideosResponse;
+
+public class SearchVideosHandlerTest extends LambdaTest {
+	SearchVideosHandler obj;
+
+	@Before
+	public void setUp() throws Exception {
+		obj = new SearchVideosHandler();
+	}
+
+	@Test
+	public void testSearchWithTranscript() {
+		SearchVideosRequest req = new SearchVideosRequest("Jim", "");
+		SearchVideosResponse res = obj.handleRequest(req, createContext("search"));
+		Assert.assertEquals(res.responseCode, 200);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testSearchWithError() {
+		SearchVideosRequest req = new SearchVideosRequest("Jim", null);
+		SearchVideosResponse res = obj.handleRequest(req, createContext("search"));
+	}
+
+}
