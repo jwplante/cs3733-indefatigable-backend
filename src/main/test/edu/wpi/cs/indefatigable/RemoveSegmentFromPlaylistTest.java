da91226e8a6ece98d14d6022b321903210ad2242
James Plante <jplante@wpi.edu>
2019-12-11 15:35:58 -0500

    Test fix

--- src/main/test/edu/wpi/cs/indefatigable/RemoveSegmentFromPlaylistTest.java
+++ src/main/test/edu/wpi/cs/indefatigable/RemoveSegmentFromPlaylistTest.java
@@ -10,20 +10,15 @@ import edu.wpi.cs.indefatigable.http.RemoveVideoFromPlaylistResponse;
 public class RemoveSegmentFromPlaylistTest extends LambdaTest{
 	
 	@Test
-	public void testAppendSegmentFromPlaylist() {
+	public void testRemoveSegmentFromPlaylist() {
         AppendSegmentToPlaylistRequest req = new AppendSegmentToPlaylistRequest("5459776a-f644-422f-916c-a15a0366d072", "32b7976c-cd4d-4fb3-805c-018053d1bf01");    
         AppendSegmentToPlaylistResponse res = new AppendSegmentToPlaylistHandler().handleRequest(req, createContext("append"));
-        
-        Assert.assertEquals(200, res.statusCode);
-	}
-	
-	@Test
-	public void testRemoveSegmentFromPlaylist() {
         RemoveVideoFromPlaylistRequest remreq = new RemoveVideoFromPlaylistRequest("5459776a-f644-422f-916c-a15a0366d072", "32b7976c-cd4d-4fb3-805c-018053d1bf01");
         RemoveVideoFromPlaylistResponse remres = new RemoveVideoFromPlaylistHandler().handleRequest(remreq, createContext("remove"));
         remres.toString();
         RemoveVideoFromPlaylistResponse other = new RemoveVideoFromPlaylistResponse("uhoh", "playlist", 200, "nooooo");
-        Assert.assertEquals(other.statusCode, remres.statusCode);
+        Assert.assertEquals(200, remres.statusCode);
+        Assert.assertEquals(200, res.statusCode);
 	}
 
 }
